b12d80aac38e23a3014bd8ce6a13611476982a8e
palabhi017 <palabhi017@gmail.com>
2022-12-14 22:11:12 +0530

    day2

---
+++ timetable/src/components/Pricepagecompo/Questions.jsx
@@ -0,0 +1,14 @@
+
+export default function Questions(){
+
+    return (
+        <div>
+            <h1 color="#333">Have additional questions?</h1>
+            <div className="Faq">
+                <div><p>BILLING / PRICING</p> <p>Billing FAQ</p></div>
+                <div><p>SPECIAL PLANS FOR</p> <p>Nonprofits or Education</p></div>
+                <div><p>ENTERPRISE PLANS</p> <p>Contact sales</p></div>
+            </div>
+        </div>
+    )
+}
---
+++ timetable/src/components/Pricepagecompo/Sponser.jsx
@@ -0,0 +1,10 @@
+
+export default function Sponser(){
+
+    return (
+        <div style={{marginTop:"30px",color:"#999",fontSize:"20px", fontWeight:"bold"}}>
+            <p  >Trusted by 300,000+ forward-thinking companies worldwide</p>
+            <img width="80%" src="https://www.airtable.com/images/about/logos-horizontal.png" alt="error" />
+        </div>
+    )
+}
